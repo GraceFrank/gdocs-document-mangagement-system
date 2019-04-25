0fdecef76b8cdad155e5906d00a66bbfc0bd1084
Grace Frank <frankgracey@gmail.com>
2019-04-25 14:43:23 +0100

    tested that roles without title cannot be created

--- tests/role/role.test.js
+++ tests/role/role.test.js
@@ -7,26 +7,39 @@ import 'babel-polyfill';
 //test Creating a role
 describe('Roles, /', () => {
   beforeEach(() => {
-    server;
+    server; //start server
   });
   afterEach(async () => {
-    await server.close();
-    await Role.remove({});
+    await server.close(); //close server
+    await Role.remove({}); //empty roles collection in db
   });
 
   describe('POST/ ', () => {
+    //test that a role must have title
     test('that the created role has title property', async () => {
       const res = await request(server)
         .post('/api/roles')
-        .send({ title: 'premiummmm' });
+        .send({ title: 'premium' });
       expect(res.body).toHaveProperty('title');
-    });
-  });
-});
-//test that a role must have title
+    }); //test end
 
-//test tht role res has title property,
-//test that if title is not provided, role should not be created
-//test that role can only be created by admin
+    //test that if title is not provided, role should not be created
+    test('roles with empty title cannot be created', async () => {
+      const res = await request(server)
+        .post('/api/roles')
+        .send({ title: '' });
+      expect(res.status).toBe(400);
+    }); //test end
+
+    test('roles without title cannot be created', async () => {
+      const res = await request(server)
+        .post('/api/roles')
+        .send({});
+      expect(res.status).toBe(400);
+    }); //test end
+  }); //end of describe (POST)
+}); //end of describe (Roles)
+
+//test that role can only be created, updated and deleted by admin
 //test that role title is unique
-//test that admin and regular roles exist
+//roles can be viewed (admin will not be visible)
