66b64f938c850e9033e364c3fb89d432079359ec
Grace Frank <frankgracey@gmail.com>
2019-04-27 08:29:42 +0100

    created document model

---
+++ models/document.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const docSchema = new mongoose.Schema({
+  ownerId: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 1000
+  },
+  content: {
+    type: String,
+    maxlength: 100000
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+
+  modifiedAt: {
+    type: Date,
+    default: Date.now
+  },
+  access: {
+    type: String,
+    enum: ['public', 'private', 'role'],
+    default: 'public'
+  }
+});
+
+const Document = mongoose.model('documents', docSchema);
+
+export default Document;
