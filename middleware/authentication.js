fd02dca8f11377e1c3a605b22c765eda40c6d92b
Grace Frank <frankgracey@gmail.com>
2019-05-19 07:04:09 +0100

    changed error respost to object

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,12 +3,14 @@ import config from 'config';
 
 function authenticate(req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('access denied no token provided');
+  if (!token)
+    return res.status(401).send({ message: 'access denied no token provided' });
   jwt.verify(token, config.get('jwtPrivateKey'), (err, decoded) => {
     if (decoded) {
       req.user = decoded;
       next();
-    } else res.status(401).send('access denied, invalid signature');
+    } else
+      res.status(401).send({ message: 'access denied, invalid signature' });
   });
 }
 
