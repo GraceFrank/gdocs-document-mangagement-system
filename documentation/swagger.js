06748f2090c1c3b9fec2c4933a27869705a125da
Grace Frank <frankgracey@gmail.com>
2019-05-19 05:49:48 +0100

    added swagger definition for api documentation

---
+++ documentation/swagger.js
@@ -0,0 +1,31 @@
+const swaggerUi = require('swagger-ui-express');
+const jsDocs = require('swagger-jsdoc');
+const express = require('express');
+const router = express.Router();
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Gdocs Document Management Systems',
+      version: '1.1.0',
+      description: 'An online document management system where users can c',
+      contact: { email: 'frank.grace7@yahoo.com', phone: '+2348137038977' }
+    },
+    tags: [
+      { name: 'doc', description: 'Everything about the users' },
+      { name: 'User', description: 'Everything about the users' }
+    ]
+  },
+  apis: ['./documentation/users.yaml']
+};
+
+const spec = jsDocs(options);
+
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(spec));
+
+// export default function(app) {
+//   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec));
+// }
+
+module.exports = router;
