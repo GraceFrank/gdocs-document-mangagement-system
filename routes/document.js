b144054ea440460af952da1efae494f286c31298
Grace Frank <frankgracey@gmail.com>
2019-05-26 08:31:51 +0100

    added controller for documents route

--- routes/document.js
+++ routes/document.js
@@ -1,140 +1,18 @@
 import express from 'express';
 import authenticate from '../middleware/authentication';
-import validate from '../api-validations/document';
-import Document from '../models/document';
 import login from '../middleware/login';
-import Role from '../models/role';
 import validateId from '../middleware/validate-id';
-import User from '../models/user';
+import documentController from '../controller/documents';
 const router = express.Router();
 
-router.post('/', authenticate, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post('/', authenticate, documentController.post);
 
-  //search if role already exists
-  const existingDoc = await Document.findOne({ title: req.body.title });
-  if (existingDoc) return res.status(400).send('document already exists');
+router.put('/:id', [validateId, authenticate], documentController.put);
 
-  const doc = await Document.create({
-    ownerId: req.user._id,
-    title: req.body.title,
-    content: req.body.content,
-    access: req.body.access || 'public',
-    role: req.user.role
-  });
+router.get('/', login, documentController.get);
 
-  res.status(201).send(doc);
-});
+router.delete('/:id', [validateId, authenticate], documentController.delete);
 
-router.put('/:id', [validateId, authenticate], async (req, res) => {
-  //validating users input
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  //checking if document exist on db
-  const doc = await Document.findOne({ _id: req.params.id });
-  if (!doc) return res.status(404).send('document not found');
-
-  //check that doc title is unique
-  const existingDoc = await Document.findOne({ title: req.body.title });
-  if (existingDoc) return res.status(400).send('document already exists');
-
-  const update = await Document.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    {
-      new: true
-    }
-  );
-  res.send(update);
-});
-
-router.get('/', login, async (req, res) => {
-  //Todo: validate query
-  const page = req.query.page * 1;
-  const limit = req.query.limit * 1;
-
-  if (!page || !limit) res.status(400).send('invalid query');
-
-  if (!req.user) {
-    docs = await Document.find({ access: 'public' })
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .sort({ date: -1 });
-    return res.send(docs);
-  }
-
-  const admin = await Role.findOne({ title: 'admin' });
-  let docs;
-
-  if (req.user.role == admin._id.toHexString()) {
-    docs = await Document.find()
-      .or([
-        { access: 'private', ownerId: req.user._id },
-        { access: { $ne: 'private' } }
-      ])
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .sort({ date: -1 });
-    return res.send(docs);
-  }
-
-  docs = await Document.find()
-    .or([
-      { access: 'private', ownerId: req.user._id },
-      { access: 'public' },
-      { role: req.user.role, access: 'role' }
-    ])
-    .skip((page - 1) * limit)
-    .limit(limit)
-    .sort({ date: -1 });
-  return res.send(docs);
-});
-
-router.delete('/:id', [validateId, authenticate], async (req, res) => {
-  //checking if document exist on db
-  const doc = await Document.findById(req.params.id);
-  if (!doc) return res.status(404).send('document not found');
-
-  //check if user is the author of document
-  if (doc.ownerId.toHexString() != req.user._id)
-    return res.status(403).send('access denied, only author can modify docs');
-
-  const deleted = await Document.findByIdAndDelete(doc._id);
-  res.send(deleted);
-});
-
-router.get('/:id', [validateId, login], async (req, res) => {
-  const doc = await Document.findById(req.params.id);
-  if (!doc) return res.status(404).send('document not found');
-
-  class GrantAccess {
-    isAdmin() {
-      const admin = Role.findOne({ title: admin });
-      return req.user.role == admin._id;
-    }
-    public() {
-      res.send(doc);
-    }
-
-    role() {
-      if (doc.role == req.user.role || this.isAdmin()) {
-        return res.send(doc);
-      } else return res.status(403).send('unauthorized access denied');
-    }
-
-    private() {
-      if (doc.ownerId == req.user._id) {
-        return res.send(doc);
-      } else
-        return res
-          .status(403)
-          .send('unauthorized access denied, private document ');
-    }
-  }
-
-  new GrantAccess()[doc.access]();
-});
+router.get('/:id', [validateId, login], documentController.getById);
 
 export default router;
