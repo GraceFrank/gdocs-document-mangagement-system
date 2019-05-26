cd3e0a63c175641d70f54866f76cb18a552db07e
Grace Frank <frankgracey@gmail.com>
2019-05-26 08:14:04 +0100

    set timestamp to true in the documents model schema

--- models/document.js
+++ models/document.js
@@ -1,43 +1,36 @@
 import mongoose from 'mongoose';
 
-const docSchema = new mongoose.Schema({
-  ownerId: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'users'
+const docSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'users'
+    },
+    title: {
+      type: String,
+      required: true,
+      maxlength: 1000,
+      unique: true
+    },
+    content: {
+      type: String,
+      maxlength: 100000
+    },
+    access: {
+      type: String,
+      enum: ['public', 'private', 'role'],
+      default: 'public'
+    },
+    role: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'roles'
+    },
+    timestamp: { type: Number, default: new Date().getTime() }
   },
-  title: {
-    type: String,
-    required: true,
-    maxlength: 1000,
-    unique: true
-  },
-  content: {
-    type: String,
-    maxlength: 100000
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    required: true
-  },
-
-  modifiedAt: {
-    type: Date,
-    default: Date.now
-  },
-  access: {
-    type: String,
-    enum: ['public', 'private', 'role'],
-    default: 'public'
-  },
-  role: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'roles'
-  },
-  timestamp: { type: Number, default: new Date().getTime() }
-});
+  { timestamps: true }
+);
 
 const Document = mongoose.model('documents', docSchema);
 
