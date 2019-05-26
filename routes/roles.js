6d521ce43cd6644cac12a55d577032d9467d2308
Grace Frank <frankgracey@gmail.com>
2019-05-26 08:08:26 +0100

    added controller for roles route

--- routes/roles.js
+++ routes/roles.js
@@ -1,58 +1,21 @@
 import express from 'express';
-import _ from 'lodash';
-import validate from '../api-validations/role';
-import Role from '../models/role';
 import authenticate from '../middleware/authentication';
 import adminAuthorization from '../middleware/admin-authorization';
 import validateId from '../middleware/validate-id';
+import role from '../controller/role';
 
 const router = express.Router();
 
 //endpoint to create a new role
-router.post('/', [authenticate, adminAuthorization], async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post('/', [authenticate, adminAuthorization], role.post);
 
-  //search if a role with given title exist
-  const existingRole = await Role.findOne({ title: req.body.title });
-  if (existingRole) return res.status(400).send('role already exists');
+//endpoint to get all roles
+router.get('/', role.get);
 
-  const newRole = await Role.create(_.pick(req.body, 'title'));
-  return res.status(201).send(newRole);
-});
+//endpoint to get a role by its id
+router.get('/:id', validateId, role.getById);
 
-//endpoint to get all routes
-router.get('/', async (req, res) => {
-  res.send(await Role.find());
-});
-
-//endpoint to get route by its id
-router.get('/:id', validateId, async (req, res) => {
-  res.send(await Role.findById(req.params.id));
-});
-
-//endpoint to create a new role
-router.put(
-  '/:id',
-  [validateId, authenticate, adminAuthorization],
-  async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    //search if a role with given title exist
-    const existingRole = await Role.findOne({
-      title: req.body.title,
-      _id: { $ne: req.params._id }
-    });
-    if (existingRole) return res.status(409).send('role already exists');
-    //Todo: find status code for conflict
-
-    const role = await Role.findByIdAndUpdate(req.params.id, req.body, {
-      new: true
-    });
-    if (!role) return res.status(404).send('no such role');
-    return res.status(200).send(role);
-  }
-);
+//endpoint to update an existing role
+router.put('/:id', [validateId, authenticate, adminAuthorization], role.put);
 
 export default router;
