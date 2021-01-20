6266f81a09e4d29191f90acfa930567191426f6b
Grace Frank <grace.frank@centricityinsights.com>
2021-01-20 16:38:19 +0100

    changed x-auth-token header to authorisation and add http only cookie

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,15 +1,16 @@
-const jwt = require('jsonwebtoken');
-const config = require('../../config/default');
+const jwt = require("jsonwebtoken");
+const config = require("../../config/default");
 
 function authenticate(req, res, next) {
-  const token = req.header('x-auth-token');
+  console.log("Cookie", req.cookie);
+  const token = req.header("Authorization");
   if (!token)
-    return res.status(401).send({ error: 'access denied no token provided' });
+    return res.status(401).send({ error: "access denied no token provided" });
   jwt.verify(token, config.privateKey, (err, decoded) => {
     if (decoded) {
       req.user = decoded;
       next();
-    } else res.status(401).send({ error: 'access denied, invalid signature' });
+    } else res.status(401).send({ error: "access denied, invalid signature" });
   });
 }
 
